55bdd16b1e205f02a404c3d2dcd734ccc2a4837a
Kaiju0 <hfaheem1973@gmail.com>
2024-02-28 23:42:41 +0800

    sdsd

--- scripts/name-flicker.js
+++ scripts/name-flicker.js
@@ -1,10 +1,25 @@
 
 const h = document.querySelector('.h');
 
-
-const mathRandom = ((Math.random() * 10)) / 2 ;
-
-function test() {
-    h.classList.toggle('.name-animation-450s');
+for (let i = 0; i < 10; i++) {
+    const mathRandom = ((Math.random() * 10)) / 6.6666666666 ;
 };
+
+function decideTiming() {
+    if(mathRandom <= 0.25) {
+        h.classList.toggle('name-025s');
+    } if(0.25 < mathRandom && mathRandom <= 0.50) {
+        h.classList.toggle('name-050s');
+    } if(0.50 < mathRandom && mathRandom <= 0.75) {
+        h.classList.toggle('name-075s');
+    } if(0.75 < mathRandom && mathRandom <= 1) {
+        h.classList.toggle('name-100s');
+    } if(1 < mathRandom && mathRandom <= 1.25) {
+        h.classList.toggle('name-125s');
+    } if(1.25 < mathRandom && mathRandom <= 1.50) {
+        h.classList.toggle('name-150s');
+    }
+};
+
+decideTiming();
 
