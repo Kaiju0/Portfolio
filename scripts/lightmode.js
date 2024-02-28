9c45f4f21e985c6f2e6feec8cd37637f35c4e828
Kaiju0 <hfaheem1973@gmail.com>
2024-02-28 17:16:56 +0800

    sdsad

--- scripts/lightmode.js
+++ scripts/lightmode.js
@@ -1,3 +1,50 @@
+
+const mainColor = '#ffffff';
+const secColor = '#000000';
+
+
+
+let clickCounter = 0;
+
+
+
 function changeColor() {
-    document.documentElement.style.setProperty('--main-color', 'white');
+    document.documentElement.style.setProperty('--main-color', mainColor);
+    document.documentElement.style.setProperty('--sec-color', secColor);
+};
+
+function revertColor() {
+    document.documentElement.style.setProperty('--main-color', secColor);
+    document.documentElement.style.setProperty('--sec-color', mainColor);
+};
+
+
+const homeImage = document.querySelector('.image');
+
+
+function invertImage() {
+    
+    homeImage.classList.remove('image');
+    homeImage.classList.add('invert-image');
 }
+
+function revertImage() {
+    
+    homeImage.classList.remove('invert-image');
+    homeImage.classList.add('image');
+}
+
+
+
+function toggleColor() {
+    if (clickCounter % 2 === 0) {
+        changeColor();
+        invertImage();
+    } else {
+        revertColor();
+        revertImage();
+    }
+
+    clickCounter++;
+
+};
