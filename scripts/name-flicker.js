a487d64c7c8347ea749d122e99fc9ff700a78f67
Kaiju0 <hfaheem1973@gmail.com>
2024-03-01 22:55:30 +0800

    dssdsa

--- scripts/name-flicker.js
+++ scripts/name-flicker.js
@@ -4,44 +4,40 @@ const Name = ['h', 'h1', 'h2', 'a', 'a1', 'a2', 'a3', 'r', 'e', 'e1', 'e2','e3',
 
 function decideTiming(letter) {
     const element = document.querySelector(`.${letter}`);
-    const mathRandom = (Math.random() * 4.5);
+    const mathRandom = Math.random();
 
     if(mathRandom <= 0.25) {
         element.classList.toggle('name-025s');
-    } else if(0.25 < mathRandom && mathRandom <= 0.50) {
+    } else if(0.25 < mathRandom && mathRandom <= 0.30) {
+        element.classList.toggle('name-030s');
+    } else if(0.30 < mathRandom && mathRandom <= 0.35) {
+        element.classList.toggle('name-035s');
+    } else if(0.35 < mathRandom && mathRandom <= 0.40) {
+        element.classList.toggle('name-040s');
+    } else if(0.40 < mathRandom && mathRandom <= 0.45) {
+        element.classList.toggle('name-045s');
+    } else if(0.45 < mathRandom && mathRandom <= 0.50) {
         element.classList.toggle('name-050s');
-    } else if(0.50 < mathRandom && mathRandom <= 0.75) {
+    } else if(0.50 < mathRandom && mathRandom <= 0.55) {
+        element.classList.toggle('name-055s');
+    } else if(0.55 < mathRandom && mathRandom <= 0.60) {
+        element.classList.toggle('name-060s');
+    } else if(0.60 < mathRandom && mathRandom <= 0.65) {
+        element.classList.toggle('name-065s');
+    } else if(0.65 < mathRandom && mathRandom <= 0.70) {
+        element.classList.toggle('name-070s');
+    } else if(0.70 < mathRandom && mathRandom <= 0.75) {
         element.classList.toggle('name-075s');
-    } else if(0.75 < mathRandom && mathRandom <= 1) {
+    } else if(0.75 < mathRandom && mathRandom <= 0.80) {
+        element.classList.toggle('name-080s');
+    } else if(0.80 < mathRandom && mathRandom <= 0.85) {
+        element.classList.toggle('name-085s');
+    } else if(0.85 < mathRandom && mathRandom <= 0.90) {
+        element.classList.toggle('name-090s');
+    } else if(0.90 < mathRandom && mathRandom <= 0.95) {
+        element.classList.toggle('name-095s');
+    } else if(0.95 < mathRandom && mathRandom <= 1) {
         element.classList.toggle('name-100s');
-    } else if(1 < mathRandom && mathRandom <= 1.25) {
-        element.classList.toggle('name-125s');
-    } else if(1.25 < mathRandom && mathRandom <= 1.50) {
-        element.classList.toggle('name-150s');
-    } else if(1.50 < mathRandom && mathRandom <= 1.75) {
-        element.classList.toggle('name-175s');
-    } else if(1.75 < mathRandom && mathRandom <= 2) {
-        element.classList.toggle('name-200s');
-    } else if(2 < mathRandom && mathRandom <= 2.25) {
-        element.classList.toggle('name-225s');
-    } else if(2.25 < mathRandom && mathRandom <= 2.50) {
-        element.classList.toggle('name-250s');
-    } else if(2.50 < mathRandom && mathRandom <= 2.75) {
-        element.classList.toggle('name-275s');
-    } else if(2.75 < mathRandom && mathRandom <= 3) {
-        element.classList.toggle('name-300s');
-    } else if(3 < mathRandom && mathRandom <= 3.25) {
-        element.classList.toggle('name-325s');
-    } else if(3.25 < mathRandom && mathRandom <= 3.50) {
-        element.classList.toggle('name-350s');
-    } else if(3.50 < mathRandom && mathRandom <= 3.75) {
-        element.classList.toggle('name-375s');
-    } else if(3.75 < mathRandom && mathRandom <= 4) {
-        element.classList.toggle('name-400s');
-    } else if(4 < mathRandom && mathRandom <= 4.25) {
-        element.classList.toggle('name-425s');
-    } else if(4.25 < mathRandom && mathRandom <= 4.50) {
-        element.classList.toggle('name-450s');
     }
 };
 
