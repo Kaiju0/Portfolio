5cfeedf4b51449c3738b98a29b6dcd7a83c58e45
Kaiju0 <hfaheem1973@gmail.com>
2024-03-06 21:37:02 +0800

    sdasda

--- scripts/content-elements.js
+++ scripts/content-elements.js
@@ -2,4 +2,11 @@ function togglePicturesElements() {
     var picturesElements = document.querySelector('.pictures-elements');
     picturesElements.classList.toggle('collapsed');
   }
+
+function toggleSitesElements() {
+    var sitesElements = document.querySelector('.sites-elements');
+    sitesElements.classList.toggle('collapsed');
+  }
+
+
   
